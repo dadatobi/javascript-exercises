89fae559932ff8cc3888a4ef9d624ae2d8ba41c7
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-04 15:23:23 +0100

    Finsihed the TDD reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const myArray = str.split("");
+    myArray.reverse();
+    let newString = myArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
