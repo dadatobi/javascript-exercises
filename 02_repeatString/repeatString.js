d9b257fea367bd1082de244e03bfbc9782abda81
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-04 14:32:44 +0100

    Finished the second TDD exercise on repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num > 0) {
+        return str.repeat(num);
+    } else if (num === 0){
+        return ""
+    } else if (num < 0) {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
