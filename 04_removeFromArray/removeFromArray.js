61614866f0b20951ae40b2383046ddd44a25f8e2
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-06 11:36:16 +0100

    Finished the 4th TDD exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arrs) {
+    const array = arrs[0];
+    const newArray = [];
+    array.forEach(item => {
+        if(!arrs.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
