2b52c5ffdd1501d7c4d2e33eedc91ad85584edf5
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-06 16:56:34 +0100

    Finished the TDD tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celTemp = (num - 32) * 5/9;
+  return parseFloat(celTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let farTemp = (num * (9/5)) + 32;
+  return parseFloat(farTemp.toFixed(1));
 };
 
 // Do not edit below this line
