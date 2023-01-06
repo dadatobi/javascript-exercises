98c238dd9b743cef48605fa891b6fca03fadf5d2
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-06 12:32:07 +0100

    Finished the sumAll TDD exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    sum = 0;
+    if (a < 0 || b < 0 || isNaN(a) || isNaN(b) || typeof a ==='string' || typeof b ==='string'){
+        return `ERROR`
+    }else if (b > a) {
+        for (i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    } else if (a > b) {
+        for (i = b; i <= a; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
