06cba6baa9c07656ac86088f4a977d8a9bc3d3c5
Dada Oluwatobi <dadatobi66@gmail.com>
2023-01-26 16:16:55 +0100

    completed the tdd for the calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...arr]) {
+  let tot = 0;
+  for (i= 0; i < arr.length; i++) {
+    tot = tot + arr[i];
+  };
+  return tot;
 };
 
-const multiply = function() {
-
+const multiply = function([...arr]) {
+  let product = arr[0];
+  for (i = 1; i< arr.length; i++) {
+    product = product * arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fac = 1
+  if(num === 0) return fac;
+  if (num > 0) {
+    for (i= 1; i <= num; i++){
+      fac = fac * i;
+    }
+    return fac;
+  }
 };
 
 // Do not edit below this line
