f97c2c9a492a19a72d19905b5bf430afe4af960a
Dada Oluwatobi <dadatobi66@gmail.com>
2023-02-06 22:00:18 +0100

    completed the TDD fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0){
+        return `OOPS`
+    }else {
+        const fibArr = [0,1]
+    for(i = 1;i <= n-1 ; i++){
+        nextSequence = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
+        fibArr.push(nextSequence);
+    }
+    return fibArr[fibArr.length-1];
+    }
+    
 };
 
 // Do not edit below this line
